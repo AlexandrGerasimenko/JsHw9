94ccc828a96d3ee01d2f5e64b9154f72eb1c69fc
Aleksandr Gerasimenko <avtoelectronic1987@gmail.com>
2019-03-29 02:26:48 +0200

    2

---
+++ js/main.js
@@ -0,0 +1,167 @@
+// Завершите код функции typeMessage так, чтобы на страницу выводился один символ в секунду
+
+// function typeMessage ( message, velocity ) {
+//     var container = document.getElementById ( "demo" ) ?
+//         document.getElementById ( "demo" ) :
+//         document.body.appendChild (
+//             document.createElement ( "h3" )
+//         )
+//     container.style = `color: magenta;`
+//     ...
+// }
+
+// typeMessage ( `Welcome to the hell`, 1 )
+
+
+function typeMessage ( message, velocity ) {
+    var container = document.getElementById ( "demo" ) ?
+        document.getElementById ( "demo" ) :
+        document.body.appendChild (
+            document.createElement ( "h3" )
+        )
+    container.style = `color: magenta;`
+    var index = 0 
+    return (function() {
+      var timerId = setTimeout(function showMessage() {
+        setTimeout(showMessage, velocity * 1000);
+        index++;
+        if(index > message.length) {null} else
+        {
+         container.innerText = message.substring(0, index);
+        }
+
+      });
+    })()
+
+}
+
+typeMessage ( `Welcome to the hell`, 1 )
+
+
+// Напилите код методов setUserPresent, showPresent и showAbsent
+
+// var users = (
+//     function ( list ) {
+//         var users = []
+//         for ( var user of list )
+//             users.push ({
+//                 name: user,
+//                 present: false
+//             })
+
+//         return {
+//             setUserPresent ( userName, present ) {
+//                 ...
+//             },
+//             showPresent () {
+//                 ...
+//             },
+//             showAbsent () {
+//                 ...
+//             }
+//         }
+//     }
+// )( [ "Иван", "Дмитрий", "Степан", "Михаил" ] )
+
+// users.showAbsent()
+// После запуска кода результат в консоли должен быть таким:
+
+// Иван
+// Дмитрий
+// Степан
+// Михаил
+// а после выполнения кода:
+
+// users.setUserPresent( "Иван", "+" )
+// users.setUserPresent( "Михаил", "присутствовал" )
+// users.setUserPresent( "Степан", true )
+
+// users.showPresent()
+// результат в консоли должен быть таким:
+
+// Иван
+// Степан
+// Михаил
+
+
+var users = (
+  function ( list ) {
+      var users = []
+      for ( var user of list )
+          users.push ({
+              name: user,
+              present: false
+          })
+
+      return {
+          setUserPresent ( userName, present ) {
+            for (var user of users) {
+              // userName === user.name ? console.log(1) : console.log(2);
+              userName === user.name && (present === '+' || "присутствовал"  || present ===  true) ? user.present = true : null;
+              
+              
+              // ???????????????????????????//
+              // userName === user.name && (present === '+' || "присутствовал"  ||  true) ? user.present = true : null;
+            
+            
+            }
+          },
+          showPresent () {
+            for (var user of users) {
+              user.present === true ?console.log(user.name) : null;
+            }
+          },
+          showAbsent () {
+            for (var user of users)
+              user.present === false ? console.log(user.name) : null;
+          }
+      }
+  }
+)( [ "Иван", "Дмитрий", "Степан", "Михаил" ] )
+
+users.showAbsent();
+users.setUserPresent( "Иван", "+" )
+users.setUserPresent( "Михаил", "присутствовал" )
+users.setUserPresent( "Степан", true )
+users.showPresent();
+
+
+// Допилите код функции changeClass, которая парсит все стили страницы в поисках заданного класса, а затем меняет атрибуты стиля этого класса
+
+// let changeClass = ( classname, styleString ) => ( 
+//    ...
+// ).length > 0 ? console.log ( "found" ) : 
+//     document.head.appendChild (
+//         document.createElement ( "style" )
+//     ).textContent = `.${classname} {${styleString}}`
+// После вызова функции:
+
+// changeClass ( "second-level-menu", "background-color: red!important;" )
+// в консоли страницы, где есть элементы с классом second-level-menu, у всех элементов этого класса цвет фона должен измениться на красный
+
+var square = function(num) {
+var elem =document.createElement('div');
+document.body.appendChild(elem);
+elem.style = `
+position: relative;
+width: 200px;
+height: 200px;
+background-color:rgb(${Math.round(Math.random()*255)},${Math.round(Math.random()*255)},${Math.round(Math.random()*255)});
+border: solid 5px white;
+`
+elem.className = !(num % 2) ? "Even": "second-level-menu";
+num--;
+num < 1 ? null : square(num)
+
+}
+square(5)
+
+
+// let changeClass = ( classname, styleString ) => ( 
+  
+// ).length > 0 ? console.log ( "found" ) : 
+//    document.head.appendChild (
+//        document.createElement ( "style" )
+//    ).textContent = `.${classname} {${styleString}}`
+
+//    changeClass ( "second-level-menu", "background-color: red!important;" )
